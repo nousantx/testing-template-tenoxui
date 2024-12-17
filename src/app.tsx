8975cc1ddecba3cc310d3911b27844afd966e191
nousantx <nousantx@gmail.com>
2024-12-17 16:32:33 +0700

    feat: add darkmode toggler feature

--- src/app.tsx
+++ src/app.tsx
@@ -1,36 +1,72 @@
 import { useLayoutEffect, useState } from 'preact/hooks'
-import { init } from '@nousantx/tenoxui-styler'
+import { init, createConfig } from '@nousantx/tenoxui-styler'
 import config from '../tenoxui.config'
 
 export const App = () => {
-  const [showNav, setShowNav] = useState(true)
+  const [showNav, setShowNav] = useState(false)
+  const [darkMode, setDarkMode] = useState(() => {
+    const storedTheme = localStorage.getItem('darkMode')
+    if (storedTheme !== null) return storedTheme === 'true'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+  })
 
-  const toggleNav = () => {
-    setShowNav((prev) => !prev)
+  const toggleNav = () => setShowNav((prev) => !prev)
+
+  const toggleDarkMode = () => {
+    setDarkMode((prev) => {
+      const newMode = !prev
+      localStorage.setItem('darkMode', newMode)
+      return newMode
+    })
   }
 
   useLayoutEffect(() => {
-    init({ config })
-  }, [])
+    const tenoxuiConfig = createConfig({ ...config, isDark: darkMode })
+    init({ config: tenoxuiConfig })
+  }, [darkMode])
 
   return (
-    <>
-      <header class="flex items-center space-between relative px-2rem py-1rem">
+    <div>
+      <header class="flex items-center space-between px-2rem py-1rem fixed [r,t,l]-0 darkmode-wrapper">
         <span class="fs-1.2rem fw-600">Tnx</span>
 
-        <button class="btn" onClick={toggleNav}>
-          show nav
-        </button>
+        <div class="flex gap-8px">
+          <button class="btn" onClick={toggleNav}>
+            Show Nav
+          </button>
+          <button class="btn" onClick={toggleDarkMode}>
+            {darkMode ? 'Light Mode' : 'Dark Mode'}
+          </button>
+        </div>
 
         <div
-          class={`fixed h-100dvh p-2rem bg-red-500 t-0 l-${
+          class={`bg-neutral-50 fixed h-100dvh p-2rem shadow-xl shadow-neutral-950 t-0 l-${
             showNav ? '0' : '-100%'
-          } [transition]-1000ms`} // use direct css properties instead of shorthand
+          } [transition]-300ms`}
         >
           Hello
         </div>
       </header>
-      <main class="p-2rem h-mn-100vh">Hello World, its main content.</main>
-    </>
+
+      <main
+        class="darkmode-wrapper p-2rem pt-6rem h-mn-100vh"
+        data-child="(p:not(:first-child)): mt-1.5rem;"
+      >
+        <header>
+          <h1 class="text-xl">Test</h1>
+        </header>
+        <section class="mt-2rem">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sapiente velit
+            maiores. In, porro.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sapiente velit
+            maiores. In, porro.
+          </p>
+          <p>Hello, it's just a test page.</p>
+        </section>
+      </main>
+    </div>
   )
 }
--- tenoxui.config.ts
+++ tenoxui.config.ts
@@ -1,10 +1,16 @@
-import { createConfig } from '@nousantx/tenoxui-styler'
+import { plugins } from 'tenoxui-plugin-utility'
 
-export default createConfig({
-  property: {
+export default {
+  shorthand: {
     tra: 'transition'
   },
-  aliases: {
-    btn: '[bdr,outline]-none bg-neutral-900 br-6px px-12px h-35px text-neutral-100 tn-capitalize'
-  }
-})
+  alias: {
+    btn: '[bdr,outline]-none bg-neutral-900 hover:bg-neutral-800 [transition]-300ms br-6px px-12px h-35px text-neutral-100 tn-capitalize fw-500',
+    'darkmode-wrapper':
+      '[transition]-300ms [transition-property]-[color,backhround-color] bg-neutral-50 text-neutral-950'
+  },
+  colorOptions: {
+    reverse: true
+  },
+  plugins: [plugins.boxShadow, plugins.typography]
+}
