e43824cead5873053c37fb884f69773361af49e4
nousantx <nousantx@gmail.com>
2024-12-18 06:25:12 +0700

    feat: added new colors

--- tenoxui.config.ts
+++ tenoxui.config.ts
@@ -5,12 +5,15 @@ export default {
     tra: 'transition'
   },
   alias: {
-    btn: '[bdr,outline]-none bg-neutral-900 hover:bg-neutral-800 [transition]-300ms br-6px px-12px h-35px text-neutral-100 tn-capitalize fw-500',
-    'darkmode-wrapper':
-      '[transition]-300ms [transition-property]-[color,backhround-color] bg-neutral-50 text-neutral-950'
+    btn: '[bdr,outline]-none bg-slate-900 hover:bg-slate-800 [transition]-300ms br-6px px-12px h-35px text-slate-100 tn-capitalize fw-500',
+    wrapper:
+      '[transition]-300ms [transition-property]-[color,background-color] bg-slate-50 text-slate-950'
   },
-  colorOptions: {
-    reverse: true
+
+  color: {
+    DEFAULT: { slate: '#5d6675' },
+    dark: { primary: '#2df14c' },
+    light: { primary: '#f53089' }
   },
   plugins: [plugins.boxShadow, plugins.typography]
 }
