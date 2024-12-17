3e511363ec5882871583dbf620f510bffe548d82
nousantx <nousantx@gmail.com>
2024-12-18 06:25:37 +0700

    feat: update darkmode feature

--- src/app.tsx
+++ src/app.tsx
@@ -9,16 +9,16 @@ export const App = () => {
     if (storedTheme !== null) return storedTheme === 'true'
     return window.matchMedia('(prefers-color-scheme: dark)').matches
   })
-
   const toggleNav = () => setShowNav((prev) => !prev)
-
   const toggleDarkMode = () => {
     setDarkMode((prev) => {
       const newMode = !prev
-      localStorage.setItem('darkMode', newMode)
+      localStorage.setItem('darkMode', newMode.toString())
       return newMode
     })
   }
+  const colors = ['primary', 'slate']
+  const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
 
   useLayoutEffect(() => {
     const tenoxuiConfig = createConfig({ ...config, isDark: darkMode })
@@ -26,8 +26,11 @@ export const App = () => {
   }, [darkMode])
 
   return (
-    <div>
-      <header class="flex items-center space-between px-2rem py-1rem fixed [r,t,l]-0 darkmode-wrapper">
+    <div class="wrapper">
+      <header
+        class="flex items-center space-between px-2rem py-1rem fixed [r,t,l]-0 z-999 bg-opacity-0.3 [backdrop-filter]-[blur(4px)]"
+        data-child="(.btn): bg-opacity-1;"
+      >
         <span class="fs-1.2rem fw-600">Tnx</span>
 
         <div class="flex gap-8px">
@@ -35,12 +38,12 @@ export const App = () => {
             Show Nav
           </button>
           <button class="btn" onClick={toggleDarkMode}>
-            {darkMode ? 'Light Mode' : 'Dark Mode'}
+            {darkMode ? 'Light' : 'Dark'}
           </button>
         </div>
 
         <div
-          class={`bg-neutral-50 fixed h-100dvh p-2rem shadow-xl shadow-neutral-950 t-0 l-${
+          class={`bg-slate-50 bg-opacity-1 fixed h-100dvh p-2rem shadow-xl shadow-slate-950 t-0 l-${
             showNav ? '0' : '-100%'
           } [transition]-300ms`}
         >
@@ -49,24 +52,32 @@ export const App = () => {
       </header>
 
       <main
-        class="darkmode-wrapper p-2rem pt-6rem h-mn-100vh"
+        class="w-mx-1024px mx-auto p-2rem pt-6rem h-mn-100vh over-x-hidden [isolation]-isolate"
         data-child="(p:not(:first-child)): mt-1.5rem;"
       >
         <header>
-          <h1 class="text-xl">Test</h1>
+          <h1 class="text-xl lh-1 ls--0.025em">Not great, but works</h1>
         </header>
         <section class="mt-2rem">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sapiente velit
-            maiores. In, porro.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sapiente velit
-            maiores. In, porro.
-          </p>
           <p>Hello, it's just a test page.</p>
+          <p>Just use HTML & CSS. In the end, we just use them, nothing more.</p>
         </section>
+        {colors.map((color) => (
+          <section
+            class="pl-1rem flex flex-wrap mt-2rem"
+            data-child="(div): box-50px br-8px [transition]-300ms ml--1rem shadow-md shadow-fc-slate-950 shadow-opacity-0.2;"
+          >
+            {shades.map((shade) => (
+              <div class={`bg-${color}-${shade}`}></div>
+            ))}
+          </section>
+        ))}
       </main>
+      <footer class="p-2rem ta-center fs-0.875rem text-slate-400 lh-1.5">
+        Built with TenoxUI ✨
+        <br />
+        Copyright &copy; 2024-present NOuSantx
+      </footer>
     </div>
   )
 }
